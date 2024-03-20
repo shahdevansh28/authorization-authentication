ffcad284b274b95e12320abd223fcfedf7bb3f4d
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2024-03-20 10:56:01 +0530

    modified: Models/AppDbContext.cs

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -13,7 +13,12 @@ namespace authentication_autharization.Models
         {
             base.OnModelCreating(builder);
         }
-
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        public DbSet<ShowTime> ShowTimes { get; set; }
+        public DbSet<Theater> Theaters { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<BookingOrder> BookingOrders { get; set; }
         public DbSet<Image> Images { get; set; }
     }
 }
